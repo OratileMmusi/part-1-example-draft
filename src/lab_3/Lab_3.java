bc975e8175975e05861085d501cf201f446b6f31
orati <orati@OraMmusi>
2024-09-03 11:05:08 +0200

    eighth commit

---
+++ src/lab_3/Lab_3.java
@@ -0,0 +1,20 @@
+
+package lab_3;
+
+/**
+ *Student name: Oratile Mmusi
+ * St Number: ST10312691
+ * Description: Lab3
+ * @author orati
+ */
+public class Lab_3 {
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        
+    }
+    
+}
